18fb4a370471ec3fe40ad9e57085706f685500b6
Alex Owen-Meehan <a.owen-meehan@kainos.com>
2019-08-13 05:09:18 +0100

    Added jdbi mapper

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -1,5 +1,16 @@
 package dao;
 
-public class EmployeeDAO {
+import java.util.List;
 
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import model.Employee;
+
+public interface EmployeeDAO {
+	
+	@SqlQuery("SELECT * FROM employee")
+	@RegisterBeanMapper(Employee.class)
+	List<Employee> getAllEmployees();
+	
 }
