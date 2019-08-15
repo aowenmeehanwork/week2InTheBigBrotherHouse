832e98535c1107a37ceb2fc6d1c0a6ad88d1b3ee
Shuaib.Saddique <shuaibs@kainos.com>
2019-08-15 11:30:02 +0100

    employee pojo implemented completely with all required fields

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -2,27 +2,115 @@ package model;
 
 public class Employee {
 
-  private int employee_id;
+	private int employee_id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String addressLine;
+	private String postCode;
+	private String email;
+	private String nin;
+	private String bankSortCode;
+	private String bankAccountNo;
+	private double salary;
 
-  public Employee() {
-  }
+	public Employee() {
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
 
-  public Employee(int employee_id) {
-    this.employee_id = employee_id;
-  }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-  public int getEmployee_id() {
-    return employee_id;
-  }
+	public String getMiddleName() {
+		return middleName;
+	}
 
-  public void setEmployee_id(int employee_id) {
-    this.employee_id = employee_id;
-  }
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 
-  @Override
-  public String toString() {
-    return "Employee{" +
-        "employee_id=" + employee_id +
-        '}';
-  }
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddressLine() {
+		return addressLine;
+	}
+
+	public void setAddressLine(String addressLine) {
+		this.addressLine = addressLine;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNin() {
+		return nin;
+	}
+
+	public void setNin(String nin) {
+		this.nin = nin;
+	}
+
+	public String getBankSortCode() {
+		return bankSortCode;
+	}
+
+	public void setBankSortCode(String bankSortCode) {
+		this.bankSortCode = bankSortCode;
+	}
+
+	public String getBankAccountNo() {
+		return bankAccountNo;
+	}
+
+	public void setBankAccountNo(String bankAccountNo) {
+		this.bankAccountNo = bankAccountNo;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Employee(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public int getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee{" + "employee_id=" + employee_id + '}';
+	}
 }
