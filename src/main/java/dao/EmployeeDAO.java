f758c86f78b14a75ea1d66434a8d17adafc71951
Shuaib.Saddique <shuaibs@kainos.com>
2019-08-15 11:12:08 +0100

    updated dao with insertEmployee

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -3,8 +3,12 @@ package dao;
 import java.util.List;
 
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
+import model.City;
 import model.Employee;
 
 public interface EmployeeDAO {
@@ -13,4 +17,9 @@ public interface EmployeeDAO {
 	@RegisterBeanMapper(Employee.class)
 	List<Employee> getAllEmployees();
 	
+	@SqlUpdate("INSERT into "
+			+ "employee(first_name, middle_name, last_name, address_line, post_code, email, nin, bank_account_no, bank_sort_code, salary) "
+			+ "VALUES (:firstName, :middleName, :lastName, :addressLine, :post_Code, :email, :nin, :bankAccountNo, :bankSortCode, :salary")
+	@GetGeneratedKeys
+	long insertEmployee(@BindBean Employee employee);
 }
