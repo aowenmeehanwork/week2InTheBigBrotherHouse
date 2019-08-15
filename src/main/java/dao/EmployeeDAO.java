8c3329bd3f7216d4fe5bd4b68dbb177fb7927b8a
Shuaib.Saddique <shuaibs@kainos.com>
2019-08-15 14:45:08 +0100

    updated insertEmployee to include department id

--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -17,8 +17,8 @@ public interface EmployeeDAO {
 	List<Employee> getAllEmployees();
 	
 	@SqlUpdate("INSERT into "
-			+ "employee(first_name, middle_name, last_name, address_line, post_code, email, nin, bank_sort_code, bank_account_no, salary) "
-      + "VALUES (:e.first_name, :e.middle_name, :e.last_name, :e.address_line, :e.post_code, :e.email, :e.nin, :e.bank_sort_code, :e.bank_account_no, :e.salary)")
+			+ "employee(first_name, middle_name, last_name, address_line, post_code, email, nin, bank_sort_code, bank_account_no, salary, department_id) "
+      + "VALUES (:e.first_name, :e.middle_name, :e.last_name, :e.address_line, :e.post_code, :e.email, :e.nin, :e.bank_sort_code, :e.bank_account_no, :e.salary, :department_id)")
 	@GetGeneratedKeys
 	int insertEmployee(@BindBean("e") Employee employee);
 	// the above used to return long and it worked, so change to that if there are errors
